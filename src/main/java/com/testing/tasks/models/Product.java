7f58df5b077875c1295acd6d46b0b142b265b773
nika <troynikv@gmail.com>
2024-08-30 10:49:59 +0300

    Add model for Product

---
+++ src/main/java/com/testing/tasks/models/Product.java
@@ -0,0 +1,16 @@
+package com.testing.tasks.models;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode
+@Builder
+public class Product {
+    private final String name;
+    private final String type;
+    private final boolean exotic;
+}
